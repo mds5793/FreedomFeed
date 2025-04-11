a0dc8cd4c13955ee527486033a1bb1ae32098a81
Matt D. Swift <mdswift5793@gmail.com>
2025-04-11 11:43:19 -0400

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,49 @@
+//FreedomFeed™ and JavaScript Code, Copyright 2022 - Matthew D. Swift & SwiftMedia LLC
+
+$(document).ready(function(){
+    $(".large, .half, .quarter-wrapper").css({"animation":"translate 60s infinite linear"});
+    $(".flashcard").hover(function(){
+        $(".large, .half, .quarter-wrapper").css({"animation-play-state":"paused"});
+    }, function(){
+        $(".large, .half, .quarter-wrapper").css({"animation-play-state":"running"});
+    });
+
+    $("#options, #notifications, #messages").click(function(){
+        $(".sidebar").animate({
+            right: 'toggle',
+            width: 'toggle'
+        }, 500);
+    });
+
+    $("#search").click(function(){
+        $("#search-id").animate({
+            width: 'toggle',
+            padding: 'toggle'
+        }, 500)
+    });
+
+    $("#motd").click(function(){
+        $("#motd-id").animate({
+            width: 'toggle',
+            padding: 'toggle'
+        });
+    });
+
+    function bindEvent(){
+        $(".add-feed").one('click', function(){
+            $(".add-flashcard-popup").animate({
+                width: 'toggle',
+                height: 'toggle'
+            });
+        });
+    }
+
+    bindEvent();
+    
+    $(".cancel").click(function(){
+        $(".add-flashcard-popup").animate({
+            width: 'toggle',
+            height: 'toggle'
+        }, bindEvent());
+    });
+});
