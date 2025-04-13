d29d97e2d8070fb1c5a33c4a130d4948b9a74430
Matt D. Swift <mdswift5793@gmail.com>
2025-04-13 12:13:01 -0400

    Added Fadein/Fadeout animations when hovering over a flashcard.

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,27 @@
 //FreedomFeed™ and HTML, CSS, and JQuery Code Copyright (c) 2022-2025 Matthew D. Swift & Swift Enterprises LLC
 
 $(document).ready(function(){
+    $(".rel").hide();
+
+    //Slow "lazy-river" animation translates unpinned flashcards from left to right across feed.
     $(".large, .half, .quarter-wrapper").css({"animation":"translate 60s infinite linear"});
+
+    //Pauses translation animation when hovering over flashcard.
     $(".flashcard").hover(function(){
         $(".large, .half, .quarter-wrapper").css({"animation-play-state":"paused"});
     }, function(){
         $(".large, .half, .quarter-wrapper").css({"animation-play-state":"running"});
     });
+
+    //Fadein Animation when hovering over flashcard.
+    $('.flashcard').hover(
+        function () {
+          $(".rel").fadeIn(325);
+        }, 
+        function () {
+          $(".rel").fadeOut(325);
+        }
+    );
 
     $("#options, #notifications, #messages").click(function(){
         $(".sidebar").animate({
